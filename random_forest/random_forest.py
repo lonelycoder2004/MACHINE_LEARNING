545650711675223c4a4da1db7081d9d4e272c72c
Ashik Shaji <ashikshaji4u@gmail.com>
2025-08-16 16:21:47 +0530

    updated random forest

---
+++ random_forest/random_forest.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from sklearn.datasets import load_digits
+digits = load_digits()
+
+df = pd.DataFrame(digits.data)
+df['target'] = digits.target
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(df.drop('target', axis=1), df['target'], test_size=0.2, random_state=42)
+
+from sklearn.ensemble import RandomForestClassifier
+clf = RandomForestClassifier(n_estimators=100, random_state=42)
+clf.fit(X_train, y_train)
+
+print(clf.score(X_test, y_test))
